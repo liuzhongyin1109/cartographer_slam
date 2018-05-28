9a7d13b6a32a6b0eeea003d9e6af80955f9fd30a
LZY <liuzhongyin1109@163..com>
2018-05-28 10:01:15 +0800

    add the install scripts

---
+++ install/base_ros_install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+#build and install the protobuf
+
+cd ${HOME}/cartographer_slam/src/protobuf
+mkdir build
+cd build
+cmake -G Ninja \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -Dprotobuf_BUILD_TESTS=OFF \
+  ../cmake
+ninja
+sudo ninja install
+
+
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+# Build and install.
+catkin_make_isolated --install --use-ninja
+source install_isolated/setup.bash
